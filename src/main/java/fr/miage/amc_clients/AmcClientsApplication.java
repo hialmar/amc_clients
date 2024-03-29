f9fe0e0c5e6ea2f64d774ece5458eee8b0ccc45c
Patrice Torguet <patrice.torguet@irit.fr>
2024-03-29 16:16:28 +0100

    Ajout sécurité sur clients, comptes et composite

--- src/main/java/fr/miage/amc_clients/AmcClientsApplication.java
+++ src/main/java/fr/miage/amc_clients/AmcClientsApplication.java
@@ -1,17 +1,25 @@
 package fr.miage.amc_clients;
 
+import com.okta.spring.boot.oauth.Okta;
 import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableMethodSecurity(prePostEnabled = true)
 public class AmcClientsApplication {
 
     public static void main(String[] args) {
@@ -32,5 +40,25 @@ public class AmcClientsApplication {
             }
         });
         return observationRegistry;
+    }
+
+    // ET LA
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+
+        // process CORS annotations
+        http.cors(Customizer.withDefaults());
+
+        // force a non-empty response body for 401's to make the response more browser friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
     }
 }
