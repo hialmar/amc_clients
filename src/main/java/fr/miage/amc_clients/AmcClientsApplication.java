f4d3c8ac1c0314ba8e211acaf325aa62664b2530
Patrice Torguet <torguet@gmail.com>
2024-04-04 15:54:04 +0200

    Ajout d'un client par défaut.

--- src/main/java/fr/miage/amc_clients/AmcClientsApplication.java
+++ src/main/java/fr/miage/amc_clients/AmcClientsApplication.java
@@ -1,6 +1,9 @@
 package fr.miage.amc_clients;
 
+import fr.miage.amc_clients.entities.Client;
+import fr.miage.amc_clients.repositories.ClientRepository;
 import io.micrometer.observation.ObservationRegistry;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -12,12 +15,30 @@ import org.springframework.util.PathMatcher;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AmcClientsApplication {
+public class AmcClientsApplication implements CommandLineRunner {
+
+    public AmcClientsApplication(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AmcClientsApplication.class, args);
     }
 
+
+    private final ClientRepository clientRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        assert(clientRepository != null);
+
+        if(clientRepository.findByPrenomAndNom("Jean", "Dupond").isEmpty()) {
+            Client client = new Client();
+            client.setPrenom("Jean");
+            client.setNom("Dupond");
+            clientRepository.save(client);
+        }
+    }
 
     @Bean
     @ConditionalOnMissingBean
